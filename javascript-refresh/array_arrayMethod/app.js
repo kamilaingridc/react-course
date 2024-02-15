8cc1768862f803c9d3288d81cf03b1987abfc5cf
Kamila Ingrid De Carvalho <kamilaingridc@gmail.com>
2024-02-14 21:55:55 -0300

    update

--- javascript-refresh/array_arrayMethod/app.js
+++ javascript-refresh/array_arrayMethod/app.js
@@ -1,2 +1,11 @@
 const hobbies = ["Sports", "Cooking", "Reading"];
-console.log(hobbies[0]);
+console.log(hobbies[0]); // put the position
+
+// add a new element 
+hobbies.push("Working");
+console.log(hobbies);
+
+// procura item
+hobbies.findIndex((item) => {
+    return item === 'Sports';
+})
